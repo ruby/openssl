e24b39f376e95530ac08bbab8301873f035b487d
Kazuki Yamaguchi <k@rhe.jp>
2016-09-08 21:43:23 +0900

    asn1: fix error path in ossl_asn1_default_tag()
    rb_class_superclass() returns nil when there is no available super class
    for the class object. Since the condition of the while statement is
    incorrect, we call rb_class_superclass() against nil. This fixes the
    segfault occurs with a code:
    
      OpenSSL::ASN1::Primitive.new("abc")

--- ext/openssl/ossl_asn1.c
+++ ext/openssl/ossl_asn1.c
@@ -631,17 +631,14 @@ ossl_asn1_default_tag(VALUE obj)
     VALUE tmp_class, tag;
 
     tmp_class = CLASS_OF(obj);
-    while (tmp_class) {
+    while (!NIL_P(tmp_class)) {
 	tag = rb_hash_lookup(class_tag_map, tmp_class);
-	if (tag != Qnil) {
-       	    return NUM2INT(tag);
-      	}
-    	tmp_class = rb_class_superclass(tmp_class);
+	if (tag != Qnil)
+	    return NUM2INT(tag);
+	tmp_class = rb_class_superclass(tmp_class);
     }
     ossl_raise(eASN1Error, "universal tag for %"PRIsVALUE" not found",
 	       rb_obj_class(obj));
-
-    return -1; /* dummy */
 }
 
 static int
