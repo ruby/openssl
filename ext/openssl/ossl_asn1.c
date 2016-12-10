22a944fe650efbd9aa685505757d8362773d3d34
Kazuki Yamaguchi <k@rhe.jp>
2016-12-11 02:58:44 +0900

    asn1: allow tag number to be >= 32 for universal tag class
    There are actually high-tag universal types such as the time-of-day
    type.

--- ext/openssl/ossl_asn1.c
+++ ext/openssl/ossl_asn1.c
@@ -661,8 +661,6 @@ ossl_asn1data_initialize(VALUE self, VALUE value, VALUE tag, VALUE tag_class)
 {
     if(!SYMBOL_P(tag_class))
 	ossl_raise(eASN1Error, "invalid tag class");
-    if (tag_class == sym_UNIVERSAL && NUM2INT(tag) > 31)
-	ossl_raise(eASN1Error, "tag number for Universal too large");
     ossl_asn1_set_tag(self, tag);
     ossl_asn1_set_value(self, value);
     ossl_asn1_set_tag_class(self, tag_class);
@@ -1085,8 +1083,6 @@ ossl_asn1_initialize(int argc, VALUE *argv, VALUE self)
 	}
 	if(!SYMBOL_P(tag_class))
 	    ossl_raise(eASN1Error, "invalid tag class");
-	if (tagging == sym_IMPLICIT && NUM2INT(tag) > 31)
-	    ossl_raise(eASN1Error, "tag number for Universal too large");
     }
     else{
 	tag = INT2NUM(default_tag);
