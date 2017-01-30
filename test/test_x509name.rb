ruby/openssl

commit 34e7fe34ee326d8d0ceb594d0fe5a90c22d91c63

Use rb_obj_class() instead of CLASS_OF()
CLASS_OF() (or, rb_class_of()) may return the singleton class of the
object; so avoid using it and replace with rb_obj_class() that returns
the actual class of the object.

ext/openssl/ossl_x509name.c
@@ -372,12 +372,10 @@ ossl_x509name_cmp(VALUE self, VALUE other)
 static VALUE
 ossl_x509name_eql(VALUE self, VALUE other)
 {
-    int result;
+    if (!rb_obj_is_kind_of(other, cX509Name))
+	return Qfalse;
 
-    if(CLASS_OF(other) != cX509Name) return Qfalse;
-    result = ossl_x509name_cmp0(self, other);
-
-    return (result == 0) ? Qtrue : Qfalse;
+    return ossl_x509name_cmp0(self, other) ? Qtrue : Qfalse;
 }
 
 /*

ext/openssl/ossl_asn1.c
@@ -1264,8 +1264,8 @@ ossl_asn1cons_to_der(VALUE self)
     if (inf_length == Qtrue) {
 	VALUE ary, example;
 	constructed = 2;
-	if (CLASS_OF(self) == cASN1Sequence ||
-	    CLASS_OF(self) == cASN1Set) {
+	if (rb_obj_class(self) == cASN1Sequence ||
+	    rb_obj_class(self) == cASN1Set) {
 	    tag = ossl_asn1_default_tag(self);
 	}
 	else { /* must be a constructive encoding of a primitive value */
@@ -1294,7 +1294,7 @@ ossl_asn1cons_to_der(VALUE self)
 	}
     }
     else {
-	if (CLASS_OF(self) == cASN1Constructive)
+	if (rb_obj_class(self) == cASN1Constructive)
 	    ossl_raise(eASN1Error, "Constructive shall only be used with infinite length");
 	tag = ossl_asn1_default_tag(self);
     }

ext/openssl/ossl_bn.c
@@ -380,7 +380,7 @@ BIGNUM_BOOL1(is_odd)
 	BIGNUM *bn, *result;				\
 	VALUE obj;					\
 	GetBN(self, bn);				\
-	obj = NewBN(CLASS_OF(self));			\
+	obj = NewBN(rb_obj_class(self));		\
 	if (!(result = BN_new())) {			\
 	    ossl_raise(eBNError, NULL);			\
 	}						\
@@ -406,7 +406,7 @@ BIGNUM_1c(sqr)
 	BIGNUM *bn1, *bn2 = GetBNPtr(other), *result;	\
 	VALUE obj;					\
 	GetBN(self, bn1);				\
-	obj = NewBN(CLASS_OF(self));			\
+	obj = NewBN(rb_obj_class(self));		\
 	if (!(result = BN_new())) {			\
 	    ossl_raise(eBNError, NULL);			\
 	}						\
@@ -439,7 +439,7 @@ BIGNUM_2(sub)
 	BIGNUM *bn1, *bn2 = GetBNPtr(other), *result;		\
 	VALUE obj;						\
 	GetBN(self, bn1);					\
-	obj = NewBN(CLASS_OF(self));				\
+	obj = NewBN(rb_obj_class(self));			\
 	if (!(result = BN_new())) {				\
 	    ossl_raise(eBNError, NULL);				\
 	}							\
@@ -504,12 +504,13 @@ static VALUE
 ossl_bn_div(VALUE self, VALUE other)
 {
     BIGNUM *bn1, *bn2 = GetBNPtr(other), *r1, *r2;
-    VALUE obj1, obj2;
+    VALUE klass, obj1, obj2;
 
     GetBN(self, bn1);
 
-    obj1 = NewBN(CLASS_OF(self));
-    obj2 = NewBN(CLASS_OF(self));
+    klass = rb_obj_class(self);
+    obj1 = NewBN(klass);
+    obj2 = NewBN(klass);
     if (!(r1 = BN_new())) {
 	ossl_raise(eBNError, NULL);
     }
@@ -536,7 +537,7 @@ ossl_bn_div(VALUE self, VALUE other)
 	BIGNUM *bn3 = GetBNPtr(other2), *result;		\
 	VALUE obj;						\
 	GetBN(self, bn1);					\
-	obj = NewBN(CLASS_OF(self));				\
+	obj = NewBN(rb_obj_class(self));			\
 	if (!(result = BN_new())) {				\
 	    ossl_raise(eBNError, NULL);				\
 	}							\
@@ -639,7 +640,7 @@ ossl_bn_is_bit_set(VALUE self, VALUE bit)
 	VALUE obj;					\
 	b = NUM2INT(bits);				\
 	GetBN(self, bn);				\
-	obj = NewBN(CLASS_OF(self));			\
+	obj = NewBN(rb_obj_class(self));		\
 	if (!(result = BN_new())) {			\
 		ossl_raise(eBNError, NULL);		\
 	}						\

ext/openssl/ossl_pkey_dh.c
@@ -460,7 +460,7 @@ ossl_dh_to_public_key(VALUE self)
 
     GetDH(self, orig_dh);
     dh = DHparams_dup(orig_dh); /* err check perfomed by dh_instance */
-    obj = dh_instance(CLASS_OF(self), dh);
+    obj = dh_instance(rb_obj_class(self), dh);
     if (obj == Qfalse) {
 	DH_free(dh);
 	ossl_raise(eDHError, NULL);

ext/openssl/ossl_pkey_dsa.c
@@ -491,7 +491,7 @@ ossl_dsa_to_public_key(VALUE self)
 	(i2d_of_void *)i2d_DSAPublicKey, (d2i_of_void *)d2i_DSAPublicKey, (char *)(dsa))
     dsa = DSAPublicKey_dup(EVP_PKEY_get0_DSA(pkey));
 #undef DSAPublicKey_dup
-    obj = dsa_instance(CLASS_OF(self), dsa);
+    obj = dsa_instance(rb_obj_class(self), dsa);
     if (obj == Qfalse) {
 	DSA_free(dsa);
 	ossl_raise(eDSAError, NULL);

ext/openssl/ossl_pkey_rsa.c
@@ -620,7 +620,7 @@ ossl_rsa_to_public_key(VALUE self)
     GetPKeyRSA(self, pkey);
     /* err check performed by rsa_instance */
     rsa = RSAPublicKey_dup(EVP_PKEY_get0_RSA(pkey));
-    obj = rsa_instance(CLASS_OF(self), rsa);
+    obj = rsa_instance(rb_obj_class(self), rsa);
     if (obj == Qfalse) {
 	RSA_free(rsa);
 	ossl_raise(eRSAError, NULL);

commit bb881da3d9e743b59e4367d7e4ddaf8c1eed9805

x509name: Correct inverted return for #eql?
Commit 34e7fe34e introduced a regression that inverted the return value
of OpenSSL::X509::Name#eql?; this commit fixes Bug #13170 and restores the
original semantics.

Reference: https://bugs.ruby-lang.org/issues/13170

ext/openssl/ossl_x509name.c
@@ -375,7 +375,7 @@ ossl_x509name_eql(VALUE self, VALUE other)
     if (!rb_obj_is_kind_of(other, cX509Name))
 	return Qfalse;
 
-    return ossl_x509name_cmp0(self, other) ? Qtrue : Qfalse;
+    return ossl_x509name_cmp0(self, other) == 0 ? Qtrue : Qfalse;
 }
 
 /*

test/test_x509name.rb
@@ -358,4 +358,10 @@ class OpenSSL::TestX509Name < OpenSSL::TestCase
     name = OpenSSL::X509::Name.parse("/CN=ruby-lang.org")
     assert_equal(name.to_der, name.dup.to_der)
   end
+
+  def test_eql
+    n1 = OpenSSL::X509::Name.new([['CN', 'ruby-lang.org']])
+    n2 = OpenSSL::X509::Name.new([['CN', 'ruby-lang.org']])
+    assert(n1.eql?(n2), "[Bug #13170]")
+  end
 end