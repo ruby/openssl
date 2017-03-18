9516f9bda3e944638669b84022320caa978297d6
Kazuki Yamaguchi <k@rhe.jp>
2017-03-18 22:34:19 +0900

    pkey: implement DH#compute_key and EC#dh_compute_key in Ruby
    Use the new OpenSSL::PKey::PKey#derive instead of the raw
    {EC,}DH_compute_key(), mainly to reduce amount of the C code.

--- lib/openssl/pkey.rb
+++ lib/openssl/pkey.rb
@@ -1,3 +1,42 @@
 # frozen_string_literal: false
 module OpenSSL
+  module PKey
+    class DH
+      # :call-seq:
+      #    dh.compute_key(pub_bn) -> string
+      #
+      # Returns a String containing a shared secret computed from the other
+      # party's public value.
+      #
+      # This method is provided for backwards compatibility, and calls #derive
+      # internally.
+      #
+      # === Parameters
+      # * _pub_bn_ is a OpenSSL::BN, *not* the DH instance returned by
+      #   DH#public_key as that contains the DH parameters only.
+      def compute_key(pub_bn)
+        peer = dup
+        peer.set_key(pub_bn, nil)
+        derive(peer)
+      end
+    end
+
+    if defined?(EC)
+    class EC
+      # :call-seq:
+      #    ec.dh_compute_key(pubkey) -> string
+      #
+      # Derives a shared secret by ECDH. _pubkey_ must be an instance of
+      # OpenSSL::PKey::EC::Point and must belong to the same group.
+      #
+      # This method is provided for backwards compatibility, and calls #derive
+      # internally.
+      def dh_compute_key(pubkey)
+        peer = OpenSSL::PKey::EC.new(group)
+        peer.public_key = pubkey
+        derive(peer)
+      end
+    end
+    end
+  end
 end
