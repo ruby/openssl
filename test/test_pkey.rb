07653ef7211edf9d379e24165ec4bb3eedd2ce70
Kazuki Yamaguchi <k@rhe.jp>
2017-03-20 23:18:26 +0900

    pkey: add OpenSSL::PKey::PKey.generate_parameters and .generate_key
    Add two methods to create a PKey using the EVP interface. This can be
    useful for PKey types we don't have a dedicated class.

--- test/test_pkey.rb
+++ test/test_pkey.rb
@@ -2,6 +2,31 @@
 require_relative 'utils'
 
 class OpenSSL::TestPKey < OpenSSL::PKeyTestCase
+  def test_s_generate_parameters
+    # 512 is non-default; 1024 is used if 'dsa_paramgen_bits' is not specified
+    # with OpenSSL 1.1.0.
+    pkey = OpenSSL::PKey.generate_parameters("DSA", "dsa_paramgen_bits" => 512)
+    assert_instance_of OpenSSL::PKey::DSA, pkey
+    assert_equal 512, pkey.p.num_bits
+    assert_equal 160, pkey.q.num_bits
+    assert_equal nil, pkey.priv_key
+
+    assert_raise(OpenSSL::PKey::PKeyError) {
+      OpenSSL::PKey.generate_parameters("DSA", "invalid" => "option")
+    }
+  end
+
+  def test_s_generate_key
+    assert_raise(OpenSSL::PKey::PKeyError) {
+      # DSA key pair cannot be generated without parameters
+      OpenSSL::PKey.generate_key("DSA")
+    }
+    pkey_params = OpenSSL::PKey.generate_parameters("DSA", "dsa_paramgen_bits" => 512)
+    pkey = OpenSSL::PKey.generate_key(pkey_params)
+    assert_instance_of OpenSSL::PKey::DSA, pkey
+    assert_not_equal nil, pkey.priv_key
+  end
+
   def test_x25519
     # RFC 7748 Section 6.1
     alice_pem = <<~EOF
@@ -19,10 +44,20 @@ class OpenSSL::TestPKey < OpenSSL::PKeyTestCase
       alice = OpenSSL::PKey.read(alice_pem)
       bob = OpenSSL::PKey.read(bob_pem)
     rescue OpenSSL::PKey::PKeyError
+      # OpenSSL < 1.1.0
       pend "X25519 is not implemented"
     end
+    assert_instance_of OpenSSL::PKey::PKey, alice
     assert_equal alice_pem, alice.private_to_pem
     assert_equal bob_pem, bob.public_to_pem
     assert_equal [shared_secret].pack("H*"), alice.derive(bob)
+
+    # Generating an X25519 key
+    assert_raise(OpenSSL::PKey::PKeyError) {
+      OpenSSL::PKey.generate_parameters("X25519")
+    }
+    key = OpenSSL::PKey.generate_key("X25519")
+    assert_nothing_raised { key.derive(bob) }
+    assert_raise(OpenSSL::PKey::PKeyError) { key.sign("sha256", "data") }
   end
 end
