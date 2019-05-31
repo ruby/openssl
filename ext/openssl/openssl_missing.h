a368d7d1ad18232edb9c26f24d9e2d17f03c3f97
Jeremy Evans <code@jeremyevans.net>
2019-05-30 21:23:19 -0700

    Check for X509_STORE_get_ex_new_index function separately
    Don't assume that just because X509_STORE_set_ex_data is defined
    that the second one is defined.  Some versions of LibreSSL need
    this. See
    https://github.com/openbsd/ports/commit/23f03b0df4af7e0606fd73c551a39430234b7449

--- ext/openssl/openssl_missing.h
+++ ext/openssl/openssl_missing.h
@@ -72,6 +72,9 @@ void ossl_HMAC_CTX_free(HMAC_CTX *);
 #if !defined(HAVE_X509_STORE_SET_EX_DATA)
 #  define X509_STORE_set_ex_data(x, idx, data) \
 	CRYPTO_set_ex_data(&(x)->ex_data, (idx), (data))
+#endif
+
+#if !defined(HAVE_X509_STORE_GET_EX_NEW_INDEX)
 #  define X509_STORE_get_ex_new_index(l, p, newf, dupf, freef) \
 	CRYPTO_get_ex_new_index(CRYPTO_EX_INDEX_X509_STORE, (l), (p), \
 				(newf), (dupf), (freef))
