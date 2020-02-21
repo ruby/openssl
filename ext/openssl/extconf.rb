ec19e47a0ac520911435b5b49c0335a2ed02e144
Kazuki Yamaguchi <k@rhe.jp>
2020-02-22 05:37:01 +0900

    ts: use TS_VERIFY_CTX_set_certs instead of TS_VERIFY_CTS_set_certs
    [ This is a backport to the 2.2 branch to fix build with LibreSSL. ]
    
    OpenSSL 3.0 fixed the typo in the function name and replaced the
    current 'CTS' version with a macro.
    
    (cherry picked from commit 2be6779b08161a084a1a5d2758de21a913740b94)

--- ext/openssl/extconf.rb
+++ ext/openssl/extconf.rb
@@ -176,12 +176,15 @@ have_func("SSL_SESSION_get_protocol_version")
 have_func("TS_STATUS_INFO_get0_status")
 have_func("TS_STATUS_INFO_get0_text")
 have_func("TS_STATUS_INFO_get0_failure_info")
-have_func("TS_VERIFY_CTS_set_certs")
+have_func("TS_VERIFY_CTS_set_certs(NULL, NULL)", "openssl/ts.h")
 have_func("TS_VERIFY_CTX_set_store")
 have_func("TS_VERIFY_CTX_add_flags")
 have_func("TS_RESP_CTX_set_time_cb")
 have_func("EVP_PBE_scrypt")
 have_func("SSL_CTX_set_post_handshake_auth")
+
+# added in 3.0.0
+have_func("TS_VERIFY_CTX_set_certs(NULL, NULL)", "openssl/ts.h")
 
 Logging::message "=== Checking done. ===\n"
 
