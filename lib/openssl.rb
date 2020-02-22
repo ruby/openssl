ruby/openssl

commit 0cddb0b736c8be8de71e231a82c08efc13f580ed

Simplify handling of version constant.

lib/openssl/version.rb
@@ -1,0 +1,5 @@
+# frozen_string_literal: true
+
+module OpenSSL
+  VERSION = "2.2.0" unless defined?(VERSION)
+end

ext/openssl/ossl.c
@@ -1157,11 +1157,6 @@ Init_openssl(void)
     rb_define_singleton_method(mOSSL, "fixed_length_secure_compare", ossl_crypto_fixed_length_secure_compare, 2);
 
     /*
-     * OpenSSL ruby extension version
-     */
-    rb_define_const(mOSSL, "VERSION", rb_str_new2(OSSL_VERSION));
-
-    /*
      * Version of OpenSSL the ruby OpenSSL extension was built with
      */
     rb_define_const(mOSSL, "OPENSSL_VERSION", rb_str_new2(OPENSSL_VERSION_TEXT));

ext/openssl/ossl.h
@@ -173,7 +173,6 @@ void ossl_debug(const char *, ...);
 #ifndef OPENSSL_NO_TS
   #include "ossl_ts.h"
 #endif
-#include "ossl_version.h"
 #include "ossl_x509.h"
 #include "ossl_engine.h"
 #include "ossl_kdf.h"

ext/openssl/ossl_version.h
@@ -1,15 +1,0 @@
-/*
- * 'OpenSSL for Ruby' project
- * Copyright (C) 2001-2002  Michal Rokos <m.rokos@sh.cvut.cz>
- * All rights reserved.
- */
-/*
- * This program is licensed under the same licence as Ruby.
- * (See the file 'LICENCE'.)
- */
-#if !defined(_OSSL_VERSION_H_)
-#define _OSSL_VERSION_H_
-
-#define OSSL_VERSION "2.1.2"
-
-#endif /* _OSSL_VERSION_H_ */

openssl.gemspec
@@ -1,6 +1,9 @@
+
+require_relative 'lib/openssl/version'
+
 Gem::Specification.new do |spec|
   spec.name          = "openssl"
-  spec.version       = "2.1.2"
+  spec.version       = OpenSSL::VERSION
   spec.authors       = ["Martin Bosslet", "SHIBATA Hiroshi", "Zachary Scott", "Kazuki Yamaguchi"]
   spec.email         = ["ruby-core@ruby-lang.org"]
   spec.summary       = %q{OpenSSL provides SSL, TLS and general purpose cryptography.}

commit b68559efc7c9c08d3b343c98d3b3c9666c20d12d

lib/openssl.rb: require openssl/version.rb
The OpenSSL::VERSION constant is now defined by lib/openssl/version.rb
instead of by the extension. Add missing require statement.

Fixes: 0cddb0b736c8 ("Simplify handling of version constant.", 2019-10-31)
Reference: https://github.com/ruby/openssl/issues/347

lib/openssl.rb
@@ -21,6 +21,7 @@ require_relative 'openssl/hmac'
 require_relative 'openssl/x509'
 require_relative 'openssl/ssl'
 require_relative 'openssl/pkcs5'
+require_relative 'openssl/version'
 
 module OpenSSL
   # call-seq:

lib/openssl/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module OpenSSL
-  VERSION = "2.2.0" unless defined?(VERSION)
+  VERSION = "2.2.0"
 end