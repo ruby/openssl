890f8bfbcedfdce406f7eaef62bf9ad25c4376d1
Kazuki Yamaguchi <k@rhe.jp>
2024-03-22 19:05:40 +0900

    ssl: allow underlying socket to not implement #remote_address
    The result value is used for generating an informative error message.
    Let's just say "unsupported" if it's not available.

--- ext/openssl/ossl_ssl.c
+++ ext/openssl/ossl_ssl.c
@@ -1536,7 +1536,10 @@ ossl_ssl_s_alloc(VALUE klass)
 static VALUE
 peer_ip_address(VALUE self)
 {
-    VALUE remote_address = rb_funcall(rb_attr_get(self, id_i_io), rb_intern("remote_address"), 0);
+    VALUE io = rb_attr_get(self, id_i_io);
+    VALUE remote_address = rb_check_funcall(io, rb_intern("remote_address"), 0, NULL);
+    if (remote_address == Qundef)
+        return rb_str_new_cstr("(unsupported)");
 
     return rb_funcall(remote_address, rb_intern("inspect_sockaddr"), 0);
 }
