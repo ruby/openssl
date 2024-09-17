63e7e5f6d662276264665d3f06e528aed70055e7
Koichi Fukuda <64008381+kmfukuda@users.noreply.github.com>
2024-09-17 03:56:50 +0000

    cmac: add comments for OpenSSL::CMAC

--- ext/openssl/ossl_cmac.c
+++ ext/openssl/ossl_cmac.c
@@ -86,6 +86,12 @@ ossl_cmac_alloc(VALUE klass)
 #endif
 }
 
+/*
+ *  call-seq:
+ *    CMAC.new(key, cipher = "AES-128-CBC") -> new_cmac
+ *
+ * Returns an OpenSSL::CMAC for a message.
+ */
 static VALUE
 ossl_cmac_initialize(int argc, VALUE *argv, VALUE self)
 {
@@ -127,6 +133,7 @@ ossl_cmac_initialize(int argc, VALUE *argv, VALUE self)
 #endif
 }
 
+/* :nodoc: */
 static VALUE
 ossl_cmac_copy(VALUE self, VALUE other)
 {
@@ -162,6 +169,12 @@ ossl_cmac_copy(VALUE self, VALUE other)
 #endif
 }
 
+/*
+ *  call-seq:
+ *    update(chunk) -> self
+ *
+ * Updates +self+ with a chunk of the message.
+ */
 static VALUE
 ossl_cmac_update(VALUE self, VALUE chunk)
 {
@@ -186,6 +199,12 @@ ossl_cmac_update(VALUE self, VALUE chunk)
 #endif
 }
 
+/*
+ *  call-seq:
+ *    mac -> string
+ *
+ * Returns the MAC.
+ */
 static VALUE
 ossl_cmac_mac(VALUE self)
 {
@@ -232,8 +251,51 @@ ossl_cmac_mac(VALUE self)
 void
 Init_ossl_cmac(void)
 {
+#if 0
+    mOSSL = rb_define_module("OpenSSL");
+    eOSSLError = rb_define_class_under(mOSSL, "OpenSSLError", rb_eStandardError);
+#endif
+
+    /*
+     * Document-class: OpenSSL::CMAC
+     *
+     * OpenSSL::CMAC provides generation of \CMAC message authentication codes (MACs).
+     * The cipher algorithm to be used can be specified as a String or an OpenSSL::Cipher.
+     * OpenSSL::CMAC has a similar interface to OpenSSL::HMAC.
+     *
+     * === Examples
+     *
+     *   key = ["8e73b0f7da0e6452c810f32b809079e562f8ead2522c6b7b"].pack("H*")
+     *   message = ["6bc1bee22e409f96e93d7e117393172a"].pack("H*")
+     *
+     *   OpenSSL::CMAC.mac(key, message, "AES-192-CBC")
+     *   #=> "\x9E\x99\xA7\xBF1\xE7\x10\x90\x06b\xF6^a|Q\x84"
+     *   OpenSSL::CMAC.hexmac(key, message, "AES-192-CBC")
+     *   #=> "9e99a7bf31e710900662f65e617c5184"
+     *   OpenSSL::CMAC.base64mac(key, message, "AES-192-CBC")
+     *   #=> "npmnvzHnEJAGYvZeYXxRhA=="
+     *
+     *   cmac = OpenSSL::CMAC.new(key, OpenSSL::Cipher.new("AES-192-CBC"))
+     *   cmac.update(message[..11])
+     *   cmac.update(message[12..])
+     *   cmac.mac
+     *   #=> "\x9E\x99\xA7\xBF1\xE7\x10\x90\x06b\xF6^a|Q\x84"
+     *   cmac.hexmac
+     *   #=> "9e99a7bf31e710900662f65e617c5184"
+     *   cmac.base64mac
+     *   #=> "npmnvzHnEJAGYvZeYXxRhA=="
+     *
+     */
     cCMAC = rb_define_class_under(mOSSL, "CMAC", rb_cObject);
+
+    /*
+     * Document-class: OpenSSL::CMACError
+     *
+     * Raised when a \CMAC operation fails.
+     * See OpenSSL::CMAC.
+     */
     eCMACError = rb_define_class_under(mOSSL, "CMACError", eOSSLError);
+
     rb_define_alloc_func(cCMAC, ossl_cmac_alloc);
     rb_define_method(cCMAC, "initialize", ossl_cmac_initialize, -1);
     rb_define_method(cCMAC, "initialize_copy", ossl_cmac_copy, 1);
--- lib/openssl/cmac.rb
+++ lib/openssl/cmac.rb
@@ -2,6 +2,30 @@
 
 module OpenSSL
   class CMAC
+    # :call-seq:
+    #   == other -> true or false
+    #
+    # Returns +true+ if the other OpenSSL::CMAC has the same MAC as +self+; +false+ otherwise.
+    #
+    # This method compares two MACs of equal length in constant time.
+    #
+    # === Example
+    #
+    #   key = ["2b7e151628aed2a6abf7158809cf4f3c"].pack("H*")
+    #   message = "message"
+    #
+    #   cmac1 = OpenSSL::CMAC.new(key)
+    #   cmac2 = cmac1.dup
+    #   cmac2 == cmac1
+    #   #=> true
+    #
+    #   cmac1.update(message)
+    #   cmac2 == cmac1
+    #   #=> false
+    #
+    #   cmac2.update(message)
+    #   cmac2 == cmac1
+    #   #=> true
     def ==(other)
       return false unless CMAC === other
       return false unless self.mac.bytesize == other.mac.bytesize
@@ -9,29 +33,49 @@ module OpenSSL
       OpenSSL.fixed_length_secure_compare(self.mac, other.mac)
     end
 
+    # :call-seq:
+    #   hexmac -> string
+    #
+    # Returns the MAC as a hex string.
     def hexmac
       mac.unpack1('H*')
     end
     alias inspect hexmac
     alias to_s hexmac
 
+    # :call-seq:
+    #   base64mac -> string
+    #
+    # Returns the MAC as a base 64 string.
     def base64mac
       [mac].pack('m0')
     end
 
     class << self
+      # :call-seq:
+      #   CMAC.mac(key, message, cipher = "AES-128-CBC") -> string
+      #
+      # Returns the MAC.
       def mac(key, message, cipher = nil)
         cmac = new(key, cipher)
         cmac << message
         cmac.mac
       end
 
+      # :call-seq:
+      #   CMAC.hexmac(key, message, cipher = "AES-128-CBC") -> string
+      #
+      # Returns the MAC as a hex string.
       def hexmac(key, message, cipher = nil)
         cmac = new(key, cipher)
         cmac << message
         cmac.hexmac
       end
 
+      # :call-seq:
+      #   CMAC.base64mac(key, message, cipher = "AES-128-CBC") -> string
+      #
+      # Returns the MAC as a base 64 string.
       def base64mac(key, message, cipher = nil)
         cmac = new(key, cipher)
         cmac << message
