7cc5068ac23eed0bd22880d5edc3c2f1f38d6c99
Kazuki Yamaguchi <k@rhe.jp>
2025-11-02 00:15:06 +0900

    digest, cipher: raise ArgumentError for unknown algorithm name
    Currently, either ArgumentError or RuntimeError is raised depending on
    the context. Always raise ArgumentError, as it better describes the
    error.

--- ext/openssl/ossl_cipher.c
+++ ext/openssl/ossl_cipher.c
@@ -114,17 +114,13 @@ ossl_cipher_initialize(VALUE self, VALUE str)
 {
     EVP_CIPHER_CTX *ctx;
     const EVP_CIPHER *cipher;
-    char *name;
 
-    name = StringValueCStr(str);
     GetCipherInit(self, ctx);
     if (ctx) {
 	ossl_raise(rb_eRuntimeError, "Cipher already initialized!");
     }
+    cipher = ossl_evp_get_cipherbyname(str);
     AllocCipher(self, ctx);
-    if (!(cipher = EVP_get_cipherbyname(name))) {
-	ossl_raise(rb_eRuntimeError, "unsupported cipher algorithm (%"PRIsVALUE")", str);
-    }
     if (EVP_CipherInit_ex(ctx, cipher, NULL, NULL, NULL, -1) != 1)
 	ossl_raise(eCipherError, NULL);
 
