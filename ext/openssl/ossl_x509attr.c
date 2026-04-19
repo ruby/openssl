8ca0afb30a6d068161a5d9245f55a1579e0b0915
ndossche <7771979+ndossche@users.noreply.github.com>
2026-04-19 15:14:49 +0200

    x509attr: check for errors of sk_ASN1_TYPE_push()
    This function returns 0 on error.

--- ext/openssl/ossl_x509attr.c
+++ ext/openssl/ossl_x509attr.c
@@ -239,9 +239,12 @@ ossl_x509attr_get_value(VALUE self)
     if (!(sk = sk_ASN1_TYPE_new_null()))
         ossl_raise(eX509AttrError, "sk_new");
 
-    count = X509_ATTRIBUTE_count(attr);
-    for (i = 0; i < count; i++)
-        sk_ASN1_TYPE_push(sk, (ASN1_TYPE *)X509_ATTRIBUTE_get0_type(attr, i));
+    for (i = 0; i < count; i++) {
+        if (!sk_ASN1_TYPE_push(sk, (ASN1_TYPE *)X509_ATTRIBUTE_get0_type(attr, i))) {
+            sk_ASN1_TYPE_free(sk);
+            ossl_raise(eX509AttrError, NULL);
+        }
+    }
 
     if ((len = i2d_ASN1_SET_ANY(sk, NULL)) <= 0) {
         sk_ASN1_TYPE_free(sk);
